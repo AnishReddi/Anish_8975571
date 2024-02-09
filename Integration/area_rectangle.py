42dd3810753edaccb5d5cd4a507eb3cd61a6a61e
anish <83488342+AnishReddi@users.noreply.github.com>
2024-02-09 00:27:14 -0500

    Add files via upload

---
+++ Integration/area_rectangle.py
@@ -0,0 +1,12 @@
+def calculate_rectangle_area(length, width):
+    """
+    Calculate the area of a rectangle.
+
+    Args:
+        length (float): Length of the rectangle.
+        width (float): Width of the rectangle.
+
+    Returns:
+        float: Area of the rectangle.
+    """
+    return length * width
